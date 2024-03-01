031ccf35a5518ebdf050d0c5c21b56e8b9eec699
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-01 14:09:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 import streamlit as st
 import pandas as pd
+from io import StringIO
 
-def main():
-    # Título de la aplicación
-    st.title("Análisis de Datos de Energía")
-
-    # Carga del archivo de Excel
-    df = pd.read_excel('referencia .xlsx')
-    
-
-    if archivo_excel is not None:
-        # Lectura del archivo Excel
-        df = pd.read_excel(archivo_excel)
-
-        # Despliegue del DataFrame
-        st.subheader("Datos del archivo Excel")
-        st.write(df)
-
-        
-
-if __name__ == "__main__":
-    main()
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)    
