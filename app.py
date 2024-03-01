f35564bef81f154efe88433ebc0ba19056829c4e
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-01 14:29:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
    df1=pd.read_excel(uploaded_file)
 else:
- st.warning(“you need to upload a csv or excel file.”)
+ st.warning('you need to upload a csv or excel file.')
