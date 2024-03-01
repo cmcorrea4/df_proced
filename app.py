c09eca53a3039c7f63346da999cd668b620b5e41
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-01 14:49:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
-   df1=pd.read_excel(uploaded_file)
+   df1=pd.read_csv(uploaded_file)
+   st.write(df1)
 else:
  st.warning('you need to upload a csv or excel file.')
