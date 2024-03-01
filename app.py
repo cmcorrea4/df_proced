ddf083877c61052a4468a37b0d2e849ce3d6142f
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-01 14:53:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+
+st.title('Análisis de datos de Producción')
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
    df1=pd.read_csv(uploaded_file)
