ba678c2e9168682fcd38428a21b567509fb4cfe4
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-01 14:25:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import streamlit as st
 import pandas as pd
-from io import StringIO
-
-uploaded_file = st.file_uploader("Choose a file")
+import streamlit as st
+uploaded_file = st.file_uploader(“Choose a file”)
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-    dataframe = pd.read_excel(uploaded_file)
-    st.write(dataframe)    
+#read csv
+df1=pd.read_csv(uploaded_file)
+
+   #read xls or xlsx
+   df1=pd.read_excel(uploaded_file)
+else:
+st.warning(“you need to upload a csv or excel file.”)
